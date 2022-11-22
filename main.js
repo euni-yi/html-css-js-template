a2a0de3208a566e5e952b5e833e6ab849f9d29d1
euni-yi <113129884+euni-yi@users.noreply.github.com>
2022-11-22 11:14:45 -0700

    half video

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-// JavaScript Code
+// Animation Intero
+
+// setup canvas and cgraphics contedt
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d")
+cnv.width = 600;
+cnv.height = 400;
+
+// Global variablwes
+let rectX = 100;
+let rectSize = 80
+let rectBlue = 225;
+let rectRed = 0;
+
+
+// Main Program Loop (60fps)
+requestAnimationFrame(loop);
+function loop(){
+    // ?Update Vaqribales
+    // rectX += Math.random()*10-5;
+    if(){
+    rectX +=1;
+    rectSize += Math.random()*2-1;
+    rectBlue --;
+    rectRed++;  
+}  
+    // deraw a background
+    ctx.fillStyle = "white";
+    ctx.fillRect (0, 0, cnv.width, cnv.height);
+
+    // draw a square
+    ctx.fillStyle = "rgb("+rectRed +", 0, "+rectBlue+")";
+    ctx.fillRect(rectX, 150, rectSize , rectSize);    
+    
+    requestAnimationFrame(loop)
+}
+
+
