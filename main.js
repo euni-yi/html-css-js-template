2a8c94986b37497474ba95ada1c7aa46b909d6b6
euni-yi <113129884+euni-yi@users.noreply.github.com>
2022-12-01 10:44:06 -0700

    fnish

--- main.js
+++ main.js
@@ -12,16 +12,15 @@ let rectSize = 80
 let rectBlue = 225;
 let rectRed = 0;
 let frameCount = 0;
+let spaceKeyIsPressed = false;
 
 
 // Main Program Loop (60fps)
 requestAnimationFrame(loop);
 function loop(){
     // ?Update Vaqribales
-    // rectX += Math.random()*10-5;
-    frameCount++; 
-
-    if(frameCount>180){
+    // rectX += Math.random()*10-5
+    if(spaceKeyIsPressed){
     rectX +=1;
     rectSize += Math.random()*2-1;
     rectBlue --;
@@ -39,3 +38,19 @@ function loop(){
 }
 
 
+// event stuff
+document.addEventListener("keydown", keydownHandler)
+document.addEventListener("keyup", keyUpHandler)
+
+function keydownHandler(event){
+    if(event.code=="Space"){
+       spaceKeyIsPressed = true;  
+    }
+   
+}
+function keyUpHandler(event){
+    if(event.code=="Space"){
+       spaceKeyIsPressed = false;  
+    }
+   
+}
